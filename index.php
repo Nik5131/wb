5a2a140abcc4d7de68274058da5763964a27384f
Nik5131 <minjnt4@gmail.com>
2026-01-15 01:41:15 +0100

    Update index.php
    added crud

--- index.php
+++ index.php
@@ -17,6 +17,30 @@ function useWeapon(WeaponInterface $weapon){
 function useShip(SpaceshipInterface $ship){
     echo $ship->scan();
 }
+function createPower($type,$speed,$fuel){
+    $stmt=$conn->prepare("INSERT INTO powers (type, speed, fuel)VALUES(?,?,?) ");
+    $stmt->bind_param("sii",$type,$speed,$fuel);
+    $stmt->execute();
+    $stmt->close();
+}
+
+function getWeapons(){
+    $stmt=$conn->query("SELECT * FROM weapons");
+    return $result->fetch_all(MYSQLI_ASSOC);
+}
+function updateShip($id,$hp){
+    $stmt=$conn->prepare("UPDATE spaceships SET hp=? WHERE id=?");
+   $stmt->bind_param("ii",$hp,$id);
+    $stmt->execute();
+    $stmt->close();
+}
+function deleteShip($id){
+    $stmt=$conn->prepare("DELETE FROM spaceships WHERE id=?");
+   $stmt->bind_param("i",$id);
+    $stmt->execute();
+    $stmt->close();
+}
+
 
 $power= new Power("A Engine",400, 100);
 $weapon= new Weapon("Big Cannon",100, 50,5);
